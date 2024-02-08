6564f16b6f2f4dba206c1a477d5bd50b41e7a9c6
Kevin Mark Montesclaros <kevinmarkmontesclaros@gmail.com>
2024-02-08 14:07:32 +0800

    10. Implementing Data Validations via Test Driven Development

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'creation' do
+    before do
+      @user = User.create(email: 'kevin123@gmail.com', 
+                          password: '123456', 
+                          password_confirmation: '123456',
+                          first_name: 'Kevin',
+                          last_name: 'Montesclaros')
+    end
+
+    it 'can be created' do
+      expect(@user).to  be_valid
+    end
+
+    it 'cannot be created without first_name and last_name' do
+      @user.first_name = nil
+      @user.last_name = nil
+
+      expect(@user).to_not be_valid
+    end
+  end
 end
