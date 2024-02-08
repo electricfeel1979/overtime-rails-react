5f119a57fa7c1e9a0f58385e732be4132a61c0ad
Kevin Mark Montesclaros <kevinmarkmontesclaros@gmail.com>
2024-02-08 15:30:57 +0800

    15. Refactoring the Post Creation Process

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+    before_action :set_post, only: [:show]
+    
     def index
     end
 
@@ -7,7 +9,7 @@ class PostsController < ApplicationController
     end
 
     def create
-        @post = Post.new(params.require(:post).permit(:date, :rationale))
+        @post = Post.new(post_params)
 
         @post.save
 
@@ -15,6 +17,15 @@ class PostsController < ApplicationController
     end
 
     def show
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:date, :rationale)
+    end
+
+    def set_post
         @post = Post.find(params[:id])
     end
 end
