552990f7606543f0a809df2e2587fee9a7c600b8
Kevin Mark Montesclaros <kevinmarkmontesclaros@gmail.com>
2024-02-11 16:16:59 +0800

    36. Blocking Non Admins for Accessing the Admin Dashboard

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -31,7 +31,7 @@ describe "navigate" do
 
     describe "new" do
         it 'has a link from the homepage' do
-            visit root_path
+            visit posts_path
 
             click_link 'new_post_from_nav'
             expect(page.status_code).to eq(200)
